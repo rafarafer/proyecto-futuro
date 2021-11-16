a5938ccd00fc0106ee6f2d5af5be9854a5d57416
Paik <Paik@LAPTOP-IV2DPH2N>
2021-11-16 23:52:55 +0100

    rama publica

---
+++ src/proyecto/futuro/ProyectoFuturo.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto.futuro;
+
+/**
+ *
+ * @author Paik
+ */
+public class ProyectoFuturo {
+
+    /**
+     * @param args the command line arguments
+     */
+   
+    static String veredicto(String valor1, String valor2){
+        if (valor1.equals(valor2)) {
+        return "Bien. Pero una hora de estudio para cada módulo puede ser insuficiente.";
+        }
+        if (Integer.valueOf(valor1) < Integer.valueOf(valor2)){
+        return "Poco tiempo de estudio. Debes dedicar más tiempo.";
+        }
+        return "Ideal. Trabajas los contenidos en casa.";
+        }
+
+        public static void main(String [] args){
+        System.out.println("Estudiante = "+args[0]);
+        System.out.println(veredicto(args[1],args[2]));
+
+}
+
+}
